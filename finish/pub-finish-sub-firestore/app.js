efdc0277e9284b68d5a6682c2992f0217a7013af
hotdog <hotdog929@gmail.com>
2019-03-14 16:51:08 +0800

    pub-finish-sub-firestore local run 版完成

--- finish/pub-finish-sub-firestore/app.js
+++ finish/pub-finish-sub-firestore/app.js
@@ -1,7 +1,11 @@
 const Mongodb = require('mongodb');
 const PubSub = require('@google-cloud/pubsub');
+const Firestore = require('@google-cloud/firestore');
 
+const google_application_credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const mongodb_uri = process.env.MONGODB_URI;
+let monogdbClient;
+let mongoDb;
 
 const client = new PubSub.v1.SubscriberClient();
 
@@ -11,9 +15,38 @@ const formattedSubscription = client.subscriptionPath(project, subscription);
 
 const maxMessages = 1;
 const request = {
-  subscription: formattedSubscription,
-  maxMessages: maxMessages,
+    subscription: formattedSubscription,
+    maxMessages: maxMessages,
 };
+
+
+const fireDb = new Firestore({
+    projectId: project,
+    keyFilename: google_application_credentials
+});
+
+async function updateFirestore(examId) {
+    let exam = await mongoDb.collection("exam").findOne({_id:examId})
+    var docRef = fireDb.collection('exam').doc(exam._id);
+    await docRef.set({
+        _id: exam._id,
+        user: exam.user,
+        finish: exam.status.finish
+    });
+}
+
+async function ack(ackId) {
+    let ackRequest = {
+        subscription: formattedSubscription,
+        ackIds: [ackId]};
+    await client.acknowledge(ackRequest, {}, (err) => {
+        if(err){
+            setTimeout(ack, 1000, ackId);
+        }else{
+            console.log(`Acknowledge ${message.message.data}`);
+        }
+    });
+}
 
 async function pull() {
     try{
@@ -21,22 +54,31 @@ async function pull() {
         let message = response.receivedMessages[0]
         console.log(`Message ${message.message.data}`);
 
-        let ackRequest = {
-            subscription: formattedSubscription,
-            ackIds: [message.ackId],
-        };
-        await client.acknowledge(ackRequest);
-        console.log(`Acknowledge ${message.message.data}`);        
+        await updateFirestore(message.message.data);
+
+        ack(message.ackId);
     }catch(err){
         console.log(err);
     }
 }
 
+async function connectMongodb() {
+    monogdbClient = await MongoClient.connect(mongodb_uri, { useNewUrlParser: true });
+    mongoDb = client.db("rxdata-test");
+}
+
 async function run() {
-    while(true){
-        await pull();
+    try{
+        await connectMongodb();
+        while(true){
+            await pull();
+        }        
+    }catch(err){
+        if(monogdbClient){
+            monogdbClient.close();
+        }
+        console.log(err);
     }
 }
 
 run();
-    
