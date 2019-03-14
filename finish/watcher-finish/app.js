f03c1379467a2e9f804e70e756a74cf7264a4f43
hotdog <hotdog929@gmail.com>
2019-03-14 13:43:14 +0800

    watcher-finish 完成

--- finish/watcher-finish/app.js
+++ finish/watcher-finish/app.js
@@ -1,17 +1,42 @@
-const Mongodb = require('mongodb');
+const {MongoClient} = require('mongodb');
 const {PubSub} = require('@google-cloud/pubsub');
 
 const mongodb_uri = process.env.MONGODB_URI;
 
 const pubsub = new PubSub();
 
-async function publish(message){
-    const messageId = await pubsub.topic("pub-finish").publish(Buffer.from(message));
-    console.log(`Message ${messageId} published.`);
+function publish(message){
+    pubsub.topic("pub-finish").publish(Buffer.from(message), (err, messageId) => {
+        if(err){
+            console.log(err);
+            setTimeout(publish, 5000, message);
+        }else{
+            console.log(`Message ${messageId} published.`);
+        }
+    });
 }
 
+async function connectMongodb(){
+    let client;
+    try{
+        client = await MongoClient.connect(mongodb_uri, { useNewUrlParser: true });
+        
+        let watchCursor = client.db("rxdata-test").collection("exam").watch([
+            {$match:{operationType:"update"}},
+            {$project:{documentId:"$documentKey._id", updatedFields:{$objectToArray:"$updateDescription.updatedFields"}}},
+            {$match:{"updatedFields.k":"status.finish"}}
+        ]);
 
-publish("A");
-publish("B");
-publish("C");
-publish("D");
+        watchCursor.on("change", function(change) {
+            publish(change.documentId);
+        });
+    }catch(err){
+        console.log(err);
+        if(client){
+            client.close();
+        }
+        setTimeout(connectMongodb, 5000);
+    }
+}
+
+connectMongodb();
