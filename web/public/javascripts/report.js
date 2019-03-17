af836c6d56f30334f9ce20304c4b3fe191e6cb4e
hotdog <hotdog929@gmail.com>
2019-03-17 19:05:58 +0800

    成績單頁同步學生和分數

--- web/public/javascripts/report.js
+++ web/public/javascripts/report.js
@@ -8,10 +8,35 @@ $(function(){
   });
 
   const db = firebase.firestore();
-  
   const urlParams = new URLSearchParams(window.location.search);
-  db.collection("exam").doc(urlParams.get("id")).onSnapshot(function(snapshot){
-    console.log(snapshot.data());
+
+  let unUserSub;
+  function updateUser(exam){
+    if(!unUserSub){
+      unUserSub = db.collection("user").doc(exam.user).onSnapshot(function(userSnapshot){
+        $('#user.card .card-content').html(`<span class="user">${userSnapshot.data().name}</span>`)
+      });
+    }
+  }
+
+  function updateScore(exam){
+    $('#score.card .card-content').html(`<span class="score">${exam.score}</span>`)
+  }
+
+  function updateExam(exam){
+    if(exam){
+      if(exam.user){
+        updateUser(exam);
+      }
+
+      if(exam.correct){
+        updateScore(exam);
+      }      
+    }
+  }
+
+  const unExamSub = db.collection("exam").doc(urlParams.get("id")).onSnapshot(function(snapshot){
+    updateExam(snapshot.data());
   });
 
 });
